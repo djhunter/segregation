7e118af717bbb73659cf860f9f04d49edca5f82d
David J. Hunter <dhunter@westmont.edu>
2019-06-14 13:46:31 -0700

    Finished income segregation function. Need to loop over all counties.

--- scripts/incomeSeg.R
+++ scripts/incomeSeg.R
@@ -4,7 +4,9 @@
 require(tidyverse)
 require(tidycensus)
 require(sf)
-if(!exists("grad", mode="function")) source("grad.R")
+require(ash)
+if(!exists("grad", mode="function")) source("scripts/grad.R")
+data(fips_codes)
 
 incomeSeg <-
   function(state,
@@ -15,8 +17,52 @@ incomeSeg <-
       geography = "block group",
       state = state,
       county = county,
-      variables = "B19013_001", # median income
+      variables = "B19013_001",
+      # median income
       geometry = TRUE
     )
-    ctyMedian <- ??? TODO
+    ctyTot <- get_acs(
+      geography = "county",
+      state = state,
+      county = county,
+      variables = "B19013_001",
+      # median income
+      geometry = FALSE
+    )
+    ctyMedian <- ctyTot$estimate
+    cty %>% mutate(
+      centroid = suppressWarnings(st_centroid(geometry)),
+      X = do.call(rbind, centroid)[, 1],
+      Y = do.call(rbind, centroid)[, 2],
+      aboveMed = (estimate > ctyMedian)
+    ) ->
+      cty
+    cty %>% select(X, Y, aboveMed) %>%
+      filter(!is.na(aboveMed)) ->
+      allObs
+    st_geometry(allObs) <- NULL
+    allObs %>% filter(aboveMed) -> success
+    allRange <-
+      matrix(c(range(allObs$X), range(allObs$Y)), 2, 2, byrow = TRUE)
+    xa <- data.matrix(allObs[, 1:2])
+    xs <- data.matrix(success[, 1:2])
+    binsa <- bin2(xa, allRange, nbin)
+    binss <- bin2(xs, allRange, nbin)
+    a.hat <- ash2(binsa, m)
+    s.hat <- ash2(binss, m)
+    f.hat <- s.hat
+    f.hat$z <- f.hat$z / a.hat$z * nrow(success) / nrow(allObs)
+    f.hat$z[is.nan(f.hat$z)] <- NA ## replace all 0/0's with NA's
+    border <- contourLines(f.hat, levels = 0.5)
+    tot <- 0
+    totAG <- 0
+    for (i in seq(length(border))) {
+      g <- grad(f.hat, border[[i]]$x, border[[i]]$y)
+      magg <- sqrt(g[1,] ^ 2 + g[2,] ^ 2)
+      nmagg <- sum(!is.na(magg))
+      aveGrad <- mean(magg, na.rm = TRUE)
+      totAG <- totAG + aveGrad * nmagg
+      tot <- tot + nmagg
+    }
+    return(totAG / tot)
   }
