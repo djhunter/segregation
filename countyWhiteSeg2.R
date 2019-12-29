825162a65ce1f096f068131d1e778e733b8afb7c
David J. Hunter <dhunter@westmont.edu>
2019-12-28 16:01:40 -0800

    Started version 2 of mathmag paper. Added references, and updated table with Geary

--- countyWhiteSeg2.R
+++ countyWhiteSeg2.R
@@ -98,4 +98,4 @@ big_county_white %>% # other filters and mutates?
   big_county_raceseg
 # saveRDS(big_county_raceseg, "data/county_raceseg2.rds")
 cor(big_county_raceseg[complete.cases(big_county_raceseg),c(10:19)])
-#xtable(cor(big_county_raceseg[complete.cases(big_county_raceseg),c(15,16,17,10,12,13)]), digits=1)
+#xtable(cor(big_county_raceseg[complete.cases(big_county_raceseg),c(15,16,17,10,12,13,19)]), digits=1)
